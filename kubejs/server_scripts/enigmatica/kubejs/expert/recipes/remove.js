9d272e3ccf643f84564793f6f6223f8e841408ce
MuteTiefling <mutetiefling@gmail.com>
2021-05-01 01:37:49 -0400

    Preliminary Ars Nouveau Progression
    Have themed the crafts a bit more around the elements, with different minerals/substances representing different elements.
    
    The idea is to put a bit of a theme behind what’s used for what.
    Earth = Apatite
    Water = Lapis
    Fire = Sulfur
    Air = Cloud Bottle
    Magic = Mana Gems
    Lightning = Fluorite
    Life = Phyto-gro
    
    Made the initial Charged Snowball recipe a bit more expensive to hopefully drive people towards the lightning recipe.
    
    Arcane Stone is more generous, but with extra steps.
    
    Lots of in game documentation added.

--- kubejs/server_scripts/enigmatica/kubejs/expert/recipes/remove.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipes/remove.js
@@ -1,10 +1,14 @@
 events.listen('recipes', (event) => {
     const idRemovals = [
+        'ars_nouveau:magic_clay',
+
+        'minecraft:conduit',
+
         'pneumaticcraft:explosion_crafting/compressed_iron_ingot',
         'pneumaticcraft:explosion_crafting/compressed_iron_block'
     ];
 
-    const outputRemovals = ['create:andesite_alloy', 'tiab:timeinabottle'];
+    const outputRemovals = ['ars_nouveau:arcane_stone', 'create:andesite_alloy', 'tiab:timeinabottle'];
 
     idRemovals.forEach((id) => {
         event.remove({ id: id });
--- kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/interactio/item_fluid_transform.js
+++ kubejs/server_scripts/enigmatica/kubejs/expert/recipetypes/interactio/item_fluid_transform.js
@@ -1,1 +1,29 @@
-events.listen('recipes', (event) => {});
+events.listen('recipes', (event) => {
+    const recipes = [
+        {
+            inputs: [
+                { item: 'eidolon:enchanted_ash', count: 2 },
+                { tag: 'forge:clay', count: 1 },
+                { tag: 'forge:dusts/mana', count: 1 },
+                { tag: 'forge:dusts/lapis', count: 1 }
+            ],
+            fluid: { fluid: 'water' },
+            output: {
+                entries: [{ result: { item: 'ars_nouveau:magic_clay', count: 2 }, weight: 1 }],
+                empty_weight: 0,
+                rolls: 1
+            },
+            consume_fluid: 1.0
+        }
+    ];
+
+    recipes.forEach((recipe) => {
+        event.custom({
+            type: 'interactio:item_fluid_transform',
+            inputs: recipe.inputs,
+            output: recipe.output,
+            fluid: recipe.fluid,
+            consume_fluid: recipe.consume_fluid
+        });
+    });
+});
